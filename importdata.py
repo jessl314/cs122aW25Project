04892d32b27ea9304ca231bc52240a66e76df60a
jessl314 <jlchrysanthemum456@gmail.com>
2025-03-09 11:51:09 -0700

    added main() call and correct file_path

--- importdata.py
+++ importdata.py
@@ -28,7 +28,7 @@ def create_connection():
         return None
 
 def create_tables():
-    """executting the DDL statements to create the tables. returns True if this was successful, False otherwise"""
+    """executing the DDL statements to create the tables. returns True if this was successful, False otherwise"""
     connection = create_connection()
     if not connection:
         return False
@@ -143,11 +143,11 @@ def create_tables():
         cursor.execute(create_sessions_table)
         cursor.execute(create_reviews_table)
         print("Tables created successfully.")
+        connection.commit()
     except mysql.connector.Error as err:
         print(f"Error creating tables: {err}")
         return False
     finally:
-        connection.commit()
         cursor.close()
         connection.close()
     return True
@@ -160,19 +160,19 @@ def load_data_from_csv(file_path, table_name):
     cursor = connection.cursor()
 
     try:
-        cursor.execute("SET GLOBAL local_infile = 1;")
+        cursor.execute("SET SESSION local_infile = 1;")
         load_query = f"""
         LOAD DATA LOCAL INFILE '{file_path}' 
-        INTO TABLE {table_name} FIELDS TERMINATED BY ',' 
+        INTO TABLE {table_name} FIELDS TERMINATED BY ','
         LINES TERMINATED BY '\n' IGNORE 1 ROWS;"""
-
+        print(load_query)
         cursor.execute(load_query)
+        connection.commit()
         print(f"Data from {file_path} loaded into {table_name}")
     except mysql.connector.Error as err:
         print(f"Error creating tables: {err}")
         return False
     finally:
-        connection.commit()
         cursor.close()
         connection.close()
     return True
@@ -188,6 +188,7 @@ def reset_database():
         cursor.execute("CREATE DATABASE ZotStreamingcs122a")
         cursor.execute("USE ZotStreamingcs122a")
         print("Database reset successfully")
+        connection.commit()
     except mysql.connector.Error as err:
         print(f"Error creating tables: {err}")
         return False
@@ -195,11 +196,6 @@ def reset_database():
         cursor.close()
         connection.close()
     return True
-
-
-
-    
-
 
 
 
@@ -219,14 +215,24 @@ def import_data(folder_name):
     if not connection:
         return False
     cursor = connection.cursor()
+    print('a')
 
     try:
+        folder_path = os.path.join(folder_name, "test_data")
+        if not os.path.exists(folder_path):
+            print(f"Folder not found: {folder_path}")
+            return False
         for csv_file in os.listdir(folder_name):
             if csv_file.endswith('.csv'):
                 table_name = csv_file.replace(".csv", "")
+                
                 file_path = os.path.join(folder_name, csv_file)
+                if not os.path.exists(file_path):
+                    print(f"File not found: {file_path}")
+                    return False
                 # change file_path to csv_file
                 success = load_data_from_csv(file_path,table_name)
+                print('b')
                 if not success:
                     return False
         connection.commit()
